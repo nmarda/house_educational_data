4c36f53e0a2b7f9fdb8ca72d317b540b345e2373
Nik Marda <nmarda@stanford.edu>
2018-12-13 14:11:06 -0600

    updated data

--- combine.py
+++ combine.py
@@ -15,7 +15,7 @@ for congress in range(107, 114):
 				line[length] = str(congress)
 			all_entries.append(line)
 			#print(line)
-print(all_entries)
+#print(all_entries)
 
 for i in range(0, len(all_entries)):
 	if (i < len(all_entries)):
@@ -27,13 +27,42 @@ for i in range(0, len(all_entries)):
 				all_entries.remove(all_entries[j])
 		all_entries[i][length] = congress_array
 
+for i in range(0, len(all_entries)):
+	print(all_entries[i][1])
+	if (all_entries[i][1]).find("M.P.") > 0:
+		all_entries[i][10] = 1
+
+for congress in range(107, 114):
+	count = 0
+	jd_count = 0
+	non_jd_count = 0
+	no_postgrad = 0
+	for i in range(0, len(all_entries)):
+		hasJD = False
+		hasNonJD = False
+		if (str(congress) in all_entries[i][length]):
+			count += 1
+			if (all_entries[i][5] == '1'):
+				jd_count += 1
+				hasJD = True
+			non_jd_degrees = 0
+			for j in range(6, length):
+				non_jd_degrees += int(all_entries[i][j])
+			if (non_jd_degrees > 0):
+				non_jd_count += 1
+				hasNonJD = True
+			if (not hasJD and not hasNonJD):
+				no_postgrad += 1
+	print(str(congress), "JDs:", jd_count, "Non-JDs:", non_jd_count, "No postgrad:", no_postgrad, "count:", count)
+
+
+
 with open('bios_combined.csv', mode='w') as writeFile:
-    fieldnames = ['Name', 'Bio', 'Identifier', 'B.A.', 'B.S.', 'J.D.', 'Ph.D.', 'M.B.A', 'M.A.', 'M.S.', 'M.P.H.', 'M.D.', 'D.M.D.', 'Congress']
+    fieldnames = ['Name', 'Identifier', 'Bio', 'B.A.', 'B.S.', 'J.D.', 'Ph.D.', 'M.B.A', 'M.A.', 'M.S.', 'M.P._.', 'M.D.', 'D.M.D.', 'Congress']
     writer = csv.DictWriter(writeFile, fieldnames = fieldnames)
     writer.writeheader()
     for entry in all_entries:
-        writer.writerow({'Name': entry[0], 'Identifier': entry[1], 'Bio': entry[2], 'B.A.': entry[3], 'B.S.': entry[4], 'J.D.': entry[5], 'Ph.D.': entry[6], 'M.B.A': entry[7], 'M.A.': entry[8], 'M.S.': entry[9], 'M.P.H.': entry[10], 'M.D.': entry[11], 'D.M.D.': entry[12], 'Congress': entry[13]}) 
-
+        writer.writerow({'Name': entry[0], 'Identifier': entry[1], 'Bio': entry[2], 'B.A.': entry[3], 'B.S.': entry[4], 'J.D.': entry[5], 'Ph.D.': entry[6], 'M.B.A': entry[7], 'M.A.': entry[8], 'M.S.': entry[9], 'M.P._.': entry[10], 'M.D.': entry[11], 'D.M.D.': entry[12], 'Congress': entry[13]}) 
 
 
 
